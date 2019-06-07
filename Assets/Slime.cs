42de45abdc1ae156cdb21926a2923bef5533b59e
Roberto Santiso <robertosantisobalea@gmail.com>
2019-06-07 13:36:34 +0100

    nueva animacion tensa pulsar dos lados contrarios tensa al slime el slime se para cuando está tenso pulsar cualquier tecla no hará que el slime se mueva

--- Assets/Slime.cs
+++ Assets/Slime.cs
@@ -22,27 +22,11 @@ public class Slime : MonoBehaviour
         velocidadDiagonal = velocidad* 0.7f;
         moviendose = false;
     }
-
-    void Update()
+    void movimientoCruz()
     {
-        moviendose = false;
-        animador.SetBool("SlimeMI", false);
-        animador.SetBool("SlimeMD", false);
-        animador.SetBool("SlimeMA", false);
-        animador.SetBool("SlimeMF", false);
-        animador.SetBool("SlimeMIA", false);
-        animador.SetBool("SlimeMIF", false);
-        animador.SetBool("SlimeMDA", false);
-        animador.SetBool("SlimeMDF", false);
-        animador.SetBool("SlimeIdle", true);
-        if (Input.anyKey)
-        {
-            moviendose = true;
-            animador.SetBool("SlimeIdle", false);
-        }
         if (Input.GetKey("a"))
         {
-            rb.velocity = new Vector2(-velocidad, 0)*Time.deltaTime;
+            rb.velocity = new Vector2(-velocidad, 0) * Time.deltaTime;
             animador.SetBool("SlimeMI", true);
             thisSprite.flipX = true;
         }
@@ -50,12 +34,12 @@ public class Slime : MonoBehaviour
             thisSprite.flipX = false;
         if (Input.GetKey("d"))
         {
-            rb.velocity = new Vector2(velocidad, -0)*Time.deltaTime;
+            rb.velocity = new Vector2(velocidad, -0) * Time.deltaTime;
             animador.SetBool("SlimeMD", true);
         }
         if (Input.GetKey("w"))
         {
-            rb.velocity = new Vector2(0, velocidad) *Time.deltaTime;
+            rb.velocity = new Vector2(0, velocidad) * Time.deltaTime;
             animador.SetBool("SlimeMA", true);
         }
         if (Input.GetKey("s"))
@@ -63,6 +47,9 @@ public class Slime : MonoBehaviour
             rb.velocity = new Vector2(0, -velocidad) * Time.deltaTime;
             animador.SetBool("SlimeMF", true);
         }
+    }
+    void movimientoDiagonal()
+    {
         if (Input.GetKey("a") && Input.GetKey("w"))
         {
             rb.velocity = new Vector2(-velocidadDiagonal, velocidadDiagonal) * Time.deltaTime;
@@ -83,6 +70,45 @@ public class Slime : MonoBehaviour
             rb.velocity = new Vector2(velocidadDiagonal, -velocidadDiagonal) * Time.deltaTime;
             animador.SetBool("SlimeMDF", true);
         }
+    }
+    /// <summary>
+    /// se aplica cuando se pulsan teclas fuera de lo esperado
+    /// </summary>
+    void movimientoFixed()
+    {
+        if (Input.GetKey("a") && Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(0, 0) * Time.deltaTime;
+            animador.SetBool("SlimeMI", true);
+            animador.SetBool("SlimeMD", true);
+        }
+        if (Input.GetKey("w") && Input.GetKey("s"))
+        {
+            rb.velocity = new Vector2(0, 0) * Time.deltaTime;
+            animador.SetBool("SlimeMA", true);
+            animador.SetBool("SlimeMF", true);
+        }
+    }
+    void Update()
+    {
+        moviendose = false;
+        animador.SetBool("SlimeMI", false);
+        animador.SetBool("SlimeMD", false);
+        animador.SetBool("SlimeMA", false);
+        animador.SetBool("SlimeMF", false);
+        animador.SetBool("SlimeMIA", false);
+        animador.SetBool("SlimeMIF", false);
+        animador.SetBool("SlimeMDA", false);
+        animador.SetBool("SlimeMDF", false);
+        animador.SetBool("SlimeIdle", true);
+        if (Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("w") || Input.GetKey("s"))
+        {
+            moviendose = true;
+            animador.SetBool("SlimeIdle", false);
+        }
+        movimientoCruz();
+        movimientoDiagonal();
+        movimientoFixed();
         if (moviendose == false)
         {
             rb.velocity = new Vector2(0, 0);            
