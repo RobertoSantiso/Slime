1dd7ecc29dc00479e250256218afe1abffc98fa3
Roberto Santiso <robertosantisobalea@gmail.com>
2019-06-15 22:17:42 +0100

    animaciones afk y follow camara
    tienes animaciones para 4 direcciones  quieto, la camara lo sigue

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    Vector3 player;
+    GameObject slime;
+    void Start()
+    {
+        slime = GameObject.Find("slime");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = slime.GetComponent<Rigidbody2D>().transform.position;
+        transform.position = new Vector3 (player.x, player.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -22,95 +22,115 @@ public class Slime : MonoBehaviour
         velocidadDiagonal = velocidad* 0.7f;
         moviendose = false;
     }
-    void movimientoCruz()
+    void ControlMovimiento()
+    {
+        MovimientoTenso();
+        if (!moviendose)
+        {
+            MovimientoDiagonal();
+            if (Input.GetKey("a"))
+                thisSprite.flipX = true;
+            else
+                thisSprite.flipX = false;
+        }
+            
+        
+        if (moviendose==false)
+            MovimientoCruz();
+    }
+    void MovimientoCruz()
     {
         if (Input.GetKey("a"))
         {
             rb.velocity = new Vector2(-velocidad, 0) * Time.deltaTime;
             animador.SetBool("SlimeMI", true);
-            thisSprite.flipX = true;
+            moviendose = true;
         }
-        else
-            thisSprite.flipX = false;
         if (Input.GetKey("d"))
         {
             rb.velocity = new Vector2(velocidad, -0) * Time.deltaTime;
             animador.SetBool("SlimeMD", true);
+            moviendose = true;
         }
         if (Input.GetKey("w"))
         {
             rb.velocity = new Vector2(0, velocidad) * Time.deltaTime;
             animador.SetBool("SlimeMA", true);
+            moviendose = true;
         }
         if (Input.GetKey("s"))
         {
             rb.velocity = new Vector2(0, -velocidad) * Time.deltaTime;
             animador.SetBool("SlimeMF", true);
+            moviendose = true;
         }
     }
-    void movimientoDiagonal()
+    void MovimientoDiagonal()
     {
         if (Input.GetKey("a") && Input.GetKey("w"))
         {
             rb.velocity = new Vector2(-velocidadDiagonal, velocidadDiagonal) * Time.deltaTime;
             animador.SetBool("SlimeMIA", true);
+            moviendose = true;
         }
         if (Input.GetKey("d") && Input.GetKey("w"))
         {
             rb.velocity = new Vector2(velocidadDiagonal, velocidadDiagonal) * Time.deltaTime;
             animador.SetBool("SlimeMDA", true);
+            moviendose = true;
         }
         if (Input.GetKey("a") && Input.GetKey("s"))
         {
             rb.velocity = new Vector2(-velocidadDiagonal, -velocidadDiagonal) * Time.deltaTime;
             animador.SetBool("SlimeMIF", true);
+            moviendose = true;
         }
         if (Input.GetKey("d") && Input.GetKey("s"))
         {
             rb.velocity = new Vector2(velocidadDiagonal, -velocidadDiagonal) * Time.deltaTime;
             animador.SetBool("SlimeMDF", true);
+            moviendose = true;
         }
     }
     /// <summary>
     /// se aplica cuando se pulsan teclas fuera de lo esperado
     /// </summary>
-    void movimientoFixed()
+    void MovimientoTenso()
     {
         if (Input.GetKey("a") && Input.GetKey("d"))
         {
             rb.velocity = new Vector2(0, 0) * Time.deltaTime;
-            animador.SetBool("SlimeMI", true);
-            animador.SetBool("SlimeMD", true);
+            animador.SetBool("SlimeTenso", true);
+            moviendose = true;
         }
         if (Input.GetKey("w") && Input.GetKey("s"))
         {
             rb.velocity = new Vector2(0, 0) * Time.deltaTime;
-            animador.SetBool("SlimeMA", true);
-            animador.SetBool("SlimeMF", true);
+            animador.SetBool("SlimeTenso", true);
+            moviendose = true;
         }
     }
     void Update()
     {
         moviendose = false;
-        animador.SetBool("SlimeMI", false);
-        animador.SetBool("SlimeMD", false);
-        animador.SetBool("SlimeMA", false);
-        animador.SetBool("SlimeMF", false);
-        animador.SetBool("SlimeMIA", false);
-        animador.SetBool("SlimeMIF", false);
-        animador.SetBool("SlimeMDA", false);
-        animador.SetBool("SlimeMDF", false);
-        animador.SetBool("SlimeIdle", true);
         if (Input.GetKey("a") || Input.GetKey("d") || Input.GetKey("w") || Input.GetKey("s"))
         {
-            moviendose = true;
+            animador.SetBool("SlimeMI", false);
+            animador.SetBool("SlimeMD", false);
+            animador.SetBool("SlimeMA", false);
+            animador.SetBool("SlimeMF", false);
+            animador.SetBool("SlimeMIA", false);
+            animador.SetBool("SlimeMDA", false);
+            animador.SetBool("SlimeMIF", false);
+            animador.SetBool("SlimeMDF", false);
+            animador.SetBool("SlimeTenso", false);
             animador.SetBool("SlimeIdle", false);
+            animador.SetBool("SlimeMoviendose", true);
+            ControlMovimiento();
+            
         }
-        movimientoCruz();
-        movimientoDiagonal();
-        movimientoFixed();
-        if (moviendose == false)
-        {
+        else {
+            animador.SetBool("SlimeMoviendose", false);
             rb.velocity = new Vector2(0, 0);            
             animador.SetBool("SlimeIdle", true);
         }
